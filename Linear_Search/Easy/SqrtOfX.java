2f0337e49a2108150863125703616edd9d6f4dea
Shubhash Singh <subhashsingh2059@gmail.com>
2024-03-10 10:18:56 +0530

    Finding Square Root of an Integer

---
+++ Linear_Search/Easy/SqrtOfX.java
@@ -0,0 +1,18 @@
+package Linear_Search.Easy;
+
+import java.util.Scanner;
+
+public class SqrtOfX {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println(calcSqrt(n));
+
+    }
+
+    private static int calcSqrt(int n ) {
+        return (int)(Math.sqrt(n));
+    }
+}
