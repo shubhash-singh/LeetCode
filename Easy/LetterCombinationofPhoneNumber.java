31aa77ab5f291f09e8e6c4fcfc9bfe972db84be0
Shubhash Singh <subhashsingh2059@gmail.com>
2024-03-28 10:58:56 +0530

    Checking whether a integer is a perfect square or not

--- Easy/LetterCombinationofPhoneNumber.java
+++ Easy/LetterCombinationofPhoneNumber.java
@@ -1,27 +1,27 @@
-package Easy;
-import java.util.List;
-import java.util.Scanner;
-public class LetterCombinationofPhoneNumber {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String digits = sc.next();
-        sc.close();
+// package Easy;
+// import java.util.List;
+// import java.util.Scanner;
+// public class LetterCombinationofPhoneNumber {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String digits = sc.next();
+//         sc.close();
 
-        System.out.println(LetterCombination(digits));
-    }
+//         System.out.println(LetterCombination(digits));
+//     }
 
-    private static List<String> LetterCombination(String digits) {
-        char[] two = {'a', 'b', 'c'};
-        char[] three = {'d', 'e', 'f'};
-        char[] four = {'g', 'h', 'i'};
-        char[] five = {'j', 'k', 'l'};
-        char[] six = {'m', 'n', 'o'};
-        char[] seven = {'p', 'q', 'r', 's'};
-        char[] eight = {'t', 'u', 'v'};
-        char[] nine = {'w', 'x', 'y', 'z'};
+//     private static List<String> LetterCombination(String digits) {
+//         char[] two = {'a', 'b', 'c'};
+//         char[] three = {'d', 'e', 'f'};
+//         char[] four = {'g', 'h', 'i'};
+//         char[] five = {'j', 'k', 'l'};
+//         char[] six = {'m', 'n', 'o'};
+//         char[] seven = {'p', 'q', 'r', 's'};
+//         char[] eight = {'t', 'u', 'v'};
+//         char[] nine = {'w', 'x', 'y', 'z'};
         
-        for(char digit : digits.toCharArray()) {
-            
-        }
-    }
-}
+//         for(char digit : digits.toCharArray()) {
+//             if(digit = )
+//         }
+//     }
+// }
---
+++ Easy/Valid_Perfect_Square.java
@@ -0,0 +1,20 @@
+package Easy;
+
+import java.util.Scanner;
+
+public class Valid_Perfect_Square{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(isPerfectSquare(num));
+        sc.close();
+    }
+    public static boolean isPerfectSquare(int num) {
+        double squareNum = Math.sqrt(num);
+        if(squareNum == Math.floor(squareNum)){
+            return true;
+        }
+        return false;
+    }
+}
