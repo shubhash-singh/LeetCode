4e5ee8d1efd5b035277c924c60fecf250117d7d8
Shubhash Singh <subhashsingh2059@gmail.com>
2024-04-03 20:53:32 +0530

    Finding the length of last word in a string

---
+++ Easy/LengthOfLastWord.java
@@ -0,0 +1,21 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+        lengthOfLast(s);
+    }
+    public static int lengthOfLast(String s){
+        List<String> str = new ArrayList<>();
+        str = Arrays.stream(s.strip().split(" ")).toList();
+        int n = str.size()-1;
+        return str.get(n).strip().length();
+    }
+}
