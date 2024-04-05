711b237429233d7535187a64646a400e94ade109
Shubhash Singh <subhashsingh2059@gmail.com>
2024-04-05 09:32:52 +0530

    Dividing two integer under extreme condition

---
+++ Easy/DivideTwoInteger.java
@@ -0,0 +1,23 @@
+package Easy;
+
+import java.util.Scanner;
+
+// https://leetcode.com/problems/divide-two-integers/
+public class DivideTwoInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dividend = sc.nextInt();
+        int divisor = sc.nextInt();
+        sc.close();
+        System.out.println(divideTwoInt(dividend, divisor));
+    }
+    static int divideTwoInt(int dividend, int divisor){
+        long result =  (long)dividend/divisor;
+        System.err.println(result);
+        if (result > Integer.MAX_VALUE){
+            return Integer.MAX_VALUE;
+        }
+        return (int)result;
+    }
+}
+// -2147483648
