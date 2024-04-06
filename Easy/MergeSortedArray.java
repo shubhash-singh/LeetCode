4f73506664876ca19aafcf805cd7a05331581a6a
Shubhash Singh <subhashsingh2059@gmail.com>
2024-04-06 18:58:03 +0530

    Merging two sorted list

---
+++ Easy/MergeSortedArray.java
@@ -0,0 +1,23 @@
+package Easy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// https://leetcode.com/problems/merge-sorted-array
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums1 = new int[sc.nextInt()];
+        int m = sc.nextInt();
+        int[] nums2 = new int[sc.nextInt()];
+        int n = sc.nextInt();
+        sc.close();
+        merge(nums1, m, nums2, n);
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=0; i<nums2.length;i++) {
+            nums1[m+i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+}
