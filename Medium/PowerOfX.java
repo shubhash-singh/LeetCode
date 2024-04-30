9238ffe24f897cc6722f896155acc11e5f27529f
Shubhash Singh <subhashsingh2059@gmail.com>
2024-04-30 12:59:36 +0530

    Getting power of a double at any condition

---
+++ Medium/PowerOfX.java
@@ -0,0 +1,25 @@
+class PowerOfX{
+    public static double myPow(double x, int n) {
+        if (n == 0){
+            return 1;
+        }
+        else if (n<0){
+            if (n<0){
+                x = Math.abs(x);
+                if (n==Integer.MIN_VALUE){
+                    return(1/Math.pow(x, Math.abs(n+1)));
+                }
+                return(1/Math.pow(x, Math.abs(n)));
+            }
+            else{
+                if (n==Integer.MIN_VALUE){
+                    return(1/Math.pow(x, Math.abs(n+1)));
+                }
+                return(1/Math.pow(x, Math.abs(n)));
+            }
+        }
+        else {
+            return Math.pow(n, x);
+        }
+    }
+}
