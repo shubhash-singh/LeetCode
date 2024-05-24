76af430e3d69de575d54f2a5674e0f3820dcae50
Shubhash Singh <subhashsingh2059@gmail.com>
2024-05-24 20:54:41 +0530

    FIrst bad version

---
+++ Easy/FirstBadVersion.py
@@ -0,0 +1,14 @@
+# 
+
+class Solution:
+    def isBadVersion(self, mid:int):
+        return True
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+        while left < right:
+            mid = left + (right - left) // 2
+            if self.isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
