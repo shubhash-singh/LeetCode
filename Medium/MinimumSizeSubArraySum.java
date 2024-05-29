9f40687fa6e8d15a810ece8aff1c78621f4c25b2
Shubhash Singh <subhashsingh2059@gmail.com>
2024-05-29 22:34:35 +0530

    MinimumSizeSubArraySum Solution

---
+++ Medium/MinimumSizeSubArraySum.java
@@ -0,0 +1,21 @@
+public class MinimumSizeSubArraySum {
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int start = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+        
+        for (int end = 0; end < nums.length; end++) {
+            sum += nums[end];
+            
+            while (sum >= target) {
+                minLength = Math.min(minLength, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+        }
+        
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+    
+}
