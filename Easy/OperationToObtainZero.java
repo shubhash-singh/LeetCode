973db82c1933e039dfe7e5b1305eceb5dc6e721c
Shubhash Singh <subhashsingh2059@gmail.com>
2024-05-30 12:50:19 +0530

    Count the operation to obtain zero

---
+++ Easy/OperationToObtainZero.java
@@ -0,0 +1,21 @@
+
+/**
+ * https://leetcode.com/problems/count-operations-to-obtain-zero/
+ */
+public class OperationToObtainZero {
+
+    public int countOperations(int num1, int num2) {
+        int steps = 0;
+        while(num1 != 0 && num2 != 0){
+            if(num1 >= num2){
+                num1 = num1 - num2;
+                steps++;
+            }
+            else {
+                num2 -= num1;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}
