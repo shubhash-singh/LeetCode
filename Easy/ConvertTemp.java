d1ffb77f8bab09faa34bb7b46438aa3633a406c4
Shubhash Singh <subhashsingh2059@gmail.com>
2024-07-24 01:40:18 +0530

    Convert given C to F and K

---
+++ Easy/ConvertTemp.java
@@ -0,0 +1,8 @@
+package Easy;
+
+public class ConvertTemp {
+    public double[] convertTemperature(double celsius) {
+        return new double[] {(celsius + 273.15) , ((celsius* 1.80 + 32.00))};
+        
+    }
+}
