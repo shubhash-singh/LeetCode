8dee1d22ff85b8a4d1c112c5fd52c9076d48721d
Shubhash Singh <subhashsingh2059@gmail.com>
2024-08-09 18:19:47 +0530

    is given coordinates in starting line

---
+++ Easy/IsStraightLine.java
@@ -0,0 +1,17 @@
+package Easy;
+
+// https://leetcode.com/problems/check-if-it-is-a-straight-line/
+
+public class IsStraightLine {
+    public boolean checkStraightLine(int[][] coordinates) {
+        int x1 = coordinates[0][0] ,y1 = coordinates[0][1];
+        int x2 = coordinates[1][0] , y2 = coordinates[1][1];
+        for(int i=0;i<coordinates.length;i++){
+            int x3 = coordinates[i][0], y3 = coordinates[i][1];
+            if ((y2 - y1) * (x3 - x2) != (y3 - y2) * (x2 - x1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
