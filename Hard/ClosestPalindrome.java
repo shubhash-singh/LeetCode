4f2613cd0c0eaf92ec3359ac4d7e866778e40ecb
Shubhash Singh <subhashsingh2059@gmail.com>
2024-08-25 20:09:18 +0530

    Find the closest palindrome

--- Hard/ClosestPalindrome.java
+++ Hard/ClosestPalindrome.java
@@ -1,3 +1,6 @@
+
+// https://leetcode.com/problems/find-the-closest-palindrome/
+
 public class ClosestPalindrome {
     public String nearestPalindromic(String numberStr) {
         long number = Long.parseLong(numberStr);
