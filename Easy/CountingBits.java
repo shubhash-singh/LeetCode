83d9d0877338a4f4eaa7190864dd49cc84fa2e69
Shubhash Singh <subhashsingh2059@gmail.com>
2024-09-12 12:27:30 +0530

    COunting bits

--- Easy/CountingBits.java
+++ Easy/CountingBits.java
@@ -1,29 +1,35 @@
 package Easy;
 
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class CountingBits {
     public static int[] countBits(int n) {
-        int[] result = new int[n+1];
-        for(int i=0;i<n;i++){
+        int[] result = new int[n + 1]; // Array to store results for numbers 0 to n
+
+        for (int i = 0; i <= n; i++) {
             int count = 0;
-            while(i > 0){
-                if((i & 1) != 0){
+            int num = i; // Use a copy of i to perform the bit operations
+
+            while (num > 0) {
+                if ((num & 1) != 0) { // Check if the last bit is 1
                     count++;
                 }
-                i = i >> 1;
+                num = num >> 1; // Shift right to check the next bit
             }
-            result[i] = count;
+            
+            result[i] = count; // Store the count of 1s in the result array
         }
-        return result;
 
+        return result;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(Arrays.toString(countBits(n)));
-        sc.close();
+        int n = 5; // Example input
+        int[] result = countBits(n);
+
+        // Print the result
+        for (int i = 0; i <= n; i++) {
+            System.out.print(result[i]+ " ");
+        }
     }
 }
