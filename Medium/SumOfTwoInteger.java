317f8c986e3a17f59634d689327bbd0f04509dc8
Shubhash Singh <subhashsingh2059@gmail.com>
2024-09-12 18:35:50 +0530

    Sum of two int without - or +

---
+++ Medium/SumOfTwoInteger.java
@@ -0,0 +1,13 @@
+
+// https://leetcode.com/problems/sum-of-two-integers/
+
+public class SumOfTwoInteger {
+    public int getSum(int a, int b) {
+        while( b != 0){
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+}
