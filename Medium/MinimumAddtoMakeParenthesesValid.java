53ed70d2004ec96c7068a116977d10f77fa9558f
Shubhash Singh <subhashsingh2059@gmail.com>
2024-10-09 20:30:32 +0530

    Minimum Add to Make Parentheses Valid

--- Medium/MinimumAddtoMakeParenthesesValid.java
+++ Medium/MinimumAddtoMakeParenthesesValid.java
@@ -1,21 +1,24 @@
 public class MinimumAddtoMakeParenthesesValid {
     public static int minAddToMakeValid(String s) {
-        int count = 0;
-
-        for(int i =0;i<s.length();i++){
-            if(s.charAt(i) == '['){
-                count++;
+        int open =0, mismatch=0;
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s.charAt(i)=='('){
+                open++;
             }
-            if(count > 0){
-                if(s.charAt(i) == ']'){
-                    count--;
-                }
+            else
+            {
+                if(open>0)
+                    open--;
+                else
+                    mismatch++;
             }
-        }
-        return count;
+        } 
+        return open+mismatch;   
     }
     public static void main(String[] args) {
         String s = "()))((";
         System.out.println(minAddToMakeValid(s));
+
     }
 }
