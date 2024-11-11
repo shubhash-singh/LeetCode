a0324ca31496c7ffbac8d35e5480e3c736246133
Shubhash Singh <subhashsingh2059@gmail.com>
2024-11-11 16:10:39 +0530

    Find Customer Referee

---
+++ SQL 50/Find Customer Referee.sql
@@ -0,0 +1,30 @@
+-- https://leetcode.com/problems/find-customer-referee/
+SELECT
+    name
+FROM
+    Customer
+WHERE
+    referee_id != 2 OR referee_id IS NULL;
+
+
+
+-- Customer table:
+-- +----+------+------------+
+-- | id | name | referee_id |
+-- +----+------+------------+
+-- | 1  | Will | null       |
+-- | 2  | Jane | null       |
+-- | 3  | Alex | 2          |
+-- | 4  | Bill | null       |
+-- | 5  | Zack | 1          |
+-- | 6  | Mark | 2          |
+-- +----+------+------------+
+-- Output: 
+-- +------+
+-- | name |
+-- +------+
+-- | Will |
+-- | Jane |
+-- | Bill |
+-- | Zack |
+-- +------+
