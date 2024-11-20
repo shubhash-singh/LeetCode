b8784c7e447ee1d08f5899c30cd879b1243d5256
Shubhash Singh <subhashsingh2059@gmail.com>
2024-11-20 22:28:23 +0530

    Sum of Values at Indices With K Set Bits

---
+++ Easy/SumofValuesatIndicesWithKSetBits.java
@@ -0,0 +1,16 @@
+package Easy;
+
+import java.util.List;
+
+public class SumofValuesatIndicesWithKSetBits {
+
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            if (Integer.bitCount(i) == k){
+                sum += nums.get(i);
+            }
+        }
+        return sum;
+    }
+}
