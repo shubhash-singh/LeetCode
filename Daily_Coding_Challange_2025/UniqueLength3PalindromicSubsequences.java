a08e2983158987cc7d4d0f0dee196022bd48cc99
Shubhash Singh <subhashsingh2059@gmail.com>
2025-01-04 10:18:00 +0530

    DCC 04-01-2025

---
+++ Daily_Coding_Challange_2025/UniqueLength3PalindromicSubsequences.java
@@ -0,0 +1,40 @@
+package Daily_Coding_Challange_2025;
+
+import java.util.HashSet;
+
+public class UniqueLength3PalindromicSubsequences {
+
+    public int countPalindromicSubsequence(String s) {
+        Pair arr[] = new Pair[26];
+        for (int i = 0; i < 26; i++) {
+            arr[i] = new Pair(-1,-1);
+        }
+        for(int i=0;i<s.length();i++){
+            if(arr[s.charAt(i)-'a'].firstOcc==-1){
+                arr[s.charAt(i)-'a'].firstOcc = i;
+            }
+            else
+                arr[s.charAt(i)-'a'].secondOcc=i;
+        }
+        int cnt=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].firstOcc !=-1 && arr[i].secondOcc !=-1){
+                HashSet<Character> hs= new HashSet<>();
+                for(int j=arr[i].firstOcc+1;j<arr[i].secondOcc;j++){
+                    hs.add(s.charAt(j));
+                }
+                cnt+=hs.size();
+            }
+        }
+        return cnt;
+    }
+}
+
+class Pair{
+    int firstOcc;
+    int secondOcc;
+    Pair(int firstOcc, int secondOcc ){
+        this.firstOcc=firstOcc;
+        this.secondOcc=secondOcc;
+    }
+}
