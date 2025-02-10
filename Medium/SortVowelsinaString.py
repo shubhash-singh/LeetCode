36eef0d6e8cc4c923cad519b473ac7e587be536d
Shubhash Singh <subhashsingh2059@gmail.com>
2025-02-10 10:34:41 +0530

    beats 90%

--- Medium/SortVowelsinaString.py
+++ Medium/SortVowelsinaString.py
@@ -2,13 +2,13 @@ class Solution:
     def sortVowels(self, s: str) -> str:
         vowels = "aeiouAEIOU"
         res_arr = ['`'] * len(s)
-        s_arr = list(s)
+        # s_arr = list(s)
         vowel_arr = []
-        for i in range(len(s_arr)):
-            if s_arr[i] not in vowels:
-                res_arr[i] = s_arr[i]
+        for i in range(len(s)):
+            if s[i] not in vowels:
+                res_arr[i] = s[i]
             else:
-                vowel_arr.append(s_arr[i])
+                vowel_arr.append(s[i])
         vowel_arr.sort()
         index = 0
         for i in range(len(res_arr)):
