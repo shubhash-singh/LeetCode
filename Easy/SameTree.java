9c459306a1c80ca043cabcc014b698c7418b563b
Shubhash Singh <subhashsingh2059@gmail.com>
2025-04-29 09:56:56 +0530

    Same Tree LeetCode 100 solved

---
+++ Easy/SameTree.java
@@ -0,0 +1,31 @@
+package Easy;
+
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+        }
+        
+        if (p.val != q.val) {
+            return false;
+        }
+        
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+    }
+}
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
